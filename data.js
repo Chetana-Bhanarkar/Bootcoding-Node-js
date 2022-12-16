d4cd1f6894ce1ebf1300932c7ea6841c64dd76d2
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-16 16:30:48 +0530

    Static API

---
+++ data.js
@@ -0,0 +1,7 @@
+module.exports = data;
+const data = [
+    {name : 'Chetana' , email : "chetana@gmail" , contact : '9999'},
+    {name : 'Aishwarya' , email : "Aishwarya@gmail" , contact : '8888'},
+    {name : 'Manish' , email : "Manish@gmail" , contact : '3333'},
+]
+
