3e46e71a0bae7cbca7dabb6595b60d4e4be3dac6
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2022-12-26 11:20:26 +0530

    API

--- Api.js
+++ Api.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const data = require('./data')
 http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type' : 'json'});
+    res.writeHead(200,{'content-type' : 'json'});
     res.write(JSON.stringify(data));
     res.end();
 }).listen(5000)
