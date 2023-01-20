1a5eaafab1077330da1715b4085265b110173f6e
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-01-20 11:02:29 +0530

    education system

--- Express js/educationSystem/model/courseEnrollment.ts
+++ Express js/educationSystem/model/courseEnrollment.ts
@@ -6,6 +6,7 @@ export class courseEnrollment{
     enrollment_date : Date
     enrollment_no : bigint
     student_id : bigint
+    package_id : bigint
     created_date : Date
     created_by : string
     modified_date : Date
