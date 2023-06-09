a2f88b04083e732ea2dbff12e73a84589b95e9a3
ChetanaBhanarkar <114468530+Chetana-Bhanarkar@users.noreply.github.com>
2023-06-09 16:44:01 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,6 @@
 module.exports = data;
 const data = [
     {name : 'Chetana' , email : "chetana@gmail" , contact : '9999'},
-    {name : 'Aishwarya' , email : "Aishwarya@gmail" , contact : '8888'},
-    {name : 'Manish' , email : "Manish@gmail" , contact : '3333'},
+    {name : 'Akash' , email : "Akash@gmail" , contact : '8888'},
 ]
 
